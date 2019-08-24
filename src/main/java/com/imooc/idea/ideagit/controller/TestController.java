8d7df3140bb9e4f1a06a03b9a72cc46636a7a91f
sunjialiang <sunjialiang@9fbank.com.cn>
2019-08-24 23:06:59 +0800

    dev 新增功能开发完成

---
+++ src/main/java/com/imooc/idea/ideagit/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.imooc.idea.ideagit.controller;
+
+/**
+ * @Author: 孙佳亮
+ * @Description:
+ * @Date: Created in 23:04 2019/8/24
+ * @Modified By:
+ */
+public class TestController {
+    public void test01(){
+        //开发工作完成
+    }
+}
